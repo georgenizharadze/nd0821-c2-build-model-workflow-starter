e0218c4fce2157b62981f555397f10df2025b7cb
George Nizharadze <George_Nizharadze@jabil.com>
2021-12-16 20:55:04 +0200

    Add longitude and latitude bounds in data cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -27,8 +27,8 @@ def go(args):
 
     logger.info("Cleaning data")
     df = df[df["price"].between(args.min_price, args.max_price)]
-    # idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
-    # df = df[idx]
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx]
     df['last_review'] = pd.to_datetime(df['last_review'])
 
     logger.info("Saving clean data locally")
